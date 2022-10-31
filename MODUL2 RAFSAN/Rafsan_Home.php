cb78338b4e9e09c908d7df4c6932484f37d28a92
Barajati12 <barajati@student.telkomuniversity.ac.id>
2022-11-01 05:55:05 +0700

    DEBUGGING 2

--- MODUL2 RAFSAN/Rafsan_Home.php
+++ MODUL2 RAFSAN/Rafsan_Home.php
@@ -1,209 +1,209 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-<body style="background-color:White; ">
-    <nav class="navbar-dark bg-secondary ">
-      <ul class="nav justify-content-center">
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+          <title>Document</title>
+      </head>
+      <body>
+      <body style="background-color:White; ">
+          <nav class="navbar-dark bg-secondary ">
+            <ul class="nav justify-content-center">
 
-        <li class="nav-item ">
-            <h6 style="text-align: center;color:antiquewhite">
-          <a class="nav-link active text-light" href="Rafsan_Home.php">
-            Home
-          </a>
-            </h6>
-        </li>
-        <li class="nav-item ">
-            <h6 style="text-align: center;color:antiquewhite">
-          <a class="nav-link active text-light" href="Rafsan_bookingpage.php">
-            Booking
-          </a>
-          </h6>
-        </li>
-      </ul>
-    </nav>
+              <li class="nav-item ">
+                  <h6 style="text-align: center;color:antiquewhite">
+                <a class="nav-link active text-light" href="Rafsan_Home.php">
+                  Home
+                </a>
+                  </h6>
+              </li>
+              <li class="nav-item ">
+                  <h6 style="text-align: center;color:antiquewhite">
+                <a class="nav-link active text-light" href="Rafsan_bookingpage.php">
+                  Booking
+                </a>
+                </h6>
+              </li>
+            </ul>
+          </nav>
 
-    <div class = "container style="text-align: center; padding-top: 0.5cm;">
+          <div class = "container style="text-align: center; padding-top: 0.5cm;">
 
-      <div class="class text-center mb-5 text-primary">
-        <h3 style="text-align: center;color:antiquewhite">
-            <b>WELCOME TO EAD RENT
-            </b>
-        </h3>
-        <p class="bg-secondary" style="text-align:center;color:antiquewhite">
-        <b>Find your the best deal, here!
-        </b> 
-        </p>
-
-      </div>
-      <div class="container mt-5">
-        <div class="card-deck">
-          <div class="card text-center" style="width: 15rem;">
-            <img src="rush.jfif" class="card-img-top" style="height: 10rem;">
-
-            <div class="card-body" style="text-align:left">
-              <h5 class="card-title">Toyota Rush
-              </h5>
-              <p>
-                  <b class="card-title">Rp 2.000.000 /day
+            <div class="class text-center mb-5 text-primary">
+              <h3 style="text-align: center;color:antiquewhite">
+                  <b>WELCOME TO EAD RENT
                   </b>
+              </h3>
+              <p class="bg-secondary" style="text-align:center;color:antiquewhite">
+              <b>Find your the best deal, here!
+              </b> 
               </p>
 
-              <br>
-            </br>
-                <div class="card-text" style="text-align:center"> 
-                <ul class="list-group list-group-flush">
-                    <div class="list-group-item" style="color: green;"> 
-                        <b>
-                        7 Kursi
-                        </b>
-                    </div>
-                    <li class="list-group-item" style="color: green;">
-                        <b>
-                        2000 CC
-                        </b>
-                    </li>
-                    <li class="list-group-item" style="color: red;">
-                        <b>
-                        Automatic
-                        </b>
-                    </li>
-                    <li class="list-group-item">
-                    </li>
-                </div>
             </div>
+            <div class="container mt-5">
+              <div class="kard-deck">
+                <div class="card text-center" style="width: 15rem;">
+                  <img src="rush.jfif" class="card-img-top" style="height: 10rem;">
 
-            <div class="card-footer text-center">
-              <a href="Rafsan_bookingpage.php?type=Toyota Rush" class="btn btn-primary form-control">
-                  <b>
-                  Book Now
-                  </b>
-              </a>
-              
-            </div>
-            </ul>
-          </div>
+                  <div class="card-body" style="text-align:left">
+                    <h5 class="card-title">Toyota Rush
+                    </h5>
+                    <p>
+                        <b class="card-title">Rp 2.000.000 /day
+                        </b>
+                    </p>
 
-          <div class="card text-center" style="width: 15rem;">
-            <img src="agya.jfif" class="card-img-top" style="height: 10rem;">
-            <div class="card-body" style="text-align:left">
-              
-            <h5 class="card-title"> 
-              Toyota Agya
-            </h5>
-              <p class="card-title">
-                  <b>
-                  Rp 750.000 /day
-                  </b>
-              </p> 
-              <br>
-            </br>
-
-                <div class="card-text" style="text-align:center"> 
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item" style="color: green;">
-                <b>
-                5 Kursi
-                </b>
-                </li>
-                
-                <li class="list-group-item" style="color: green;">
-                <b>
-                1000 CC
-                </b>
-                </li>
-                  
-                <li class="list-group-item" style="color: red;">
-                <b>
-                Automatic
-                </b>
-                </li>
-                  
-                <li class="list-group-item">
-                </li>
+                    <br>
+                  </br>
+                      <div class="card-text" style="text-align:center"> 
+                      <ul class="list-group list-group-flush">
+                          <div class="list-group-item" style="color: green;"> 
+                              <b>
+                              7 Kursi
+                              </b>
+                          </div>
+                          <li class="list-group-item" style="color: green;">
+                              <b>
+                              2000 CC
+                              </b>
+                          </li>
+                          <li class="list-group-item" style="color: red;">
+                              <b>
+                              Automatic
+                              </b>
+                          </li>
+                          <li class="list-group-item">
+                          </li>
+                      </div>
                   </div>
-            </div>
-            
-            <div class="card-footer text-center">
-              <a href="Rafsan_bookingpage.php?type=Toyota Agya" class="btn btn-primary form-control">
+
+                  <div class="card-footer text-center">
+                    <a href="Rafsan_bookingpage.php?type= Toyota Rush" class="btn btn-primary form-control">
+                        <b>
+                        Book Now
+                        </b>
+                    </a>
+                    
+                  </div>
+                  </ul>
+                </div>
+
+                <div class="card text-center" style="width: 15rem;">
+                  <img src="agya.jfif" class="card-img-top" style="height: 10rem;">
+                  <div class="card-body" style="text-align:left">
+                    
+                  <h5 class="card-title"> 
+                    Toyota Agya
+                  </h5>
+                    <p class="card-title">
+                        <b>
+                        Rp 750.000 /day
+                        </b>
+                    </p> 
+                    <br>
+                  </br>
+
+                      <div class="card-text" style="text-align:center"> 
+                      <ul class="list-group list-group-flush">
+                      <li class="list-group-item" style="color: green;">
+                      <b>
+                      5 Kursi
+                      </b>
+                      </li>
+                      
+                      <li class="list-group-item" style="color: green;">
+                      <b>
+                      1000 CC
+                      </b>
+                      </li>
+                        
+                      <li class="list-group-item" style="color: red;">
+                      <b>
+                      Automatic
+                      </b>
+                      </li>
+                        
+                      <li class="list-group-item">
+                      </li>
+                        </div>
+                  </div>
+                  
+                  <div class="card-footer text-center">
+                    <a href="Rafsan_bookingpage.php?type=Toyota Agya" class="btn btn-primary form-control">
+                        <b>
+                        Book Now
+                        </b>
+                    </a>
+                    
+                  </div>
+
+                </div>
+                  
+                  <div class="card text-center" style="width: 15rem;">
+                  <img src="brio.jfif" class="card-img-top" style="height: 10rem;">
+                  <div class="card-body" style="text-align:left">
+                    
+                  <h5 class="card-title">
+                  Honda Brio
+                  </h5>
+
+                  <p class="card-title">
+                      <b>
+                        Rp 300.000 /day
+                      </b>
+                  </p> 
+
+                  <br>
+              </br>
+
+                  <div class="card-text" style="text-align:center"> 
+                  
+                  <ul class="list-group list-group-flush">
+                  
+                  <li class="list-group-item" style="color:green;">
                   <b>
-                  Book Now
+                      5 Kursi
                   </b>
-              </a>
-              
-            </div>
+                  </li>
 
-          </div>
-            
-            <div class="card text-center" style="width: 15rem;">
-            <img src="brio.jfif" class="card-img-top" style="height: 10rem;">
-            <div class="card-body" style="text-align:left">
-              
-            <h5 class="card-title">
-            Honda Brio
-            </h5>
-
-            <p class="card-title">
-                <b>
-                  Rp 300.000 /day
-                </b>
-            </p> 
-
-            <br>
-        </br>
-
-            <div class="card-text" style="text-align:center"> 
-            
-            <ul class="list-group list-group-flush">
-            
-            <li class="list-group-item" style="color:green;">
-            <b>
-                5 Kursi
-            </b>
-            </li>
-
-            <li class="list-group-item" style="color:green;">
-            <b>
-                1200 CC
-            </b>
-            </li>
-
-            <li class="list-group-item" style="color:red;">
-            <b>
-                Automatic
-            </b>
-            </li>
-            
-            
-            <li class="list-group-item">
-            </li>
-            
-            </div>
-
-            <div class="card-footer text-center">
-              <a href="Rafsan_bookingpage.php?type=Honda Brio" class="btn btn-primary form-control">
+                  <li class="list-group-item" style="color:green;">
                   <b>
-                  Book Now
+                      1200 CC
                   </b>
-              </a>
-              
-            </div>
+                  </li>
 
+                  <li class="list-group-item" style="color:red;">
+                  <b>
+                      Automatic
+                  </b>
+                  </li>
+                  
+                  
+                  <li class="list-group-item">
+                  </li>
+                  
+                  </div>
+
+                  <div class="card-footer text-center">
+                    <a href="Rafsan_bookingpage.php?type=Honda Brio" class="btn btn-primary form-control">
+                        <b>
+                        Book Now
+                        </b>
+                    </a>
+                    
+                  </div>
+
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-    <br>
-    <footer style="text-align: center;color:black">
-    <b>
-        (Created by Rafsan_1202202291)
-    </b>
-</footer>
-</body>
-</html>
+          <br>
+          <footer style="text-align: center;color:black">
+          <b>
+              (Created by Rafsan_1202202291)
+          </b>
+      </footer>
+      </body>
+      </html>
--- MODUL2 RAFSAN/Rafsan_bookdetail.php
+++ MODUL2 RAFSAN/Rafsan_bookdetail.php
@@ -1,119 +1,119 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <body style="background-color:gray;">
-    <nav class="navbar-dark bg-secondary ">
-      <ul class="nav justify-content-center">
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            <body style="background-color:gray;">
+            <nav class="navbar-dark bg-secondary ">
+              <ul class="nav justify-content-center">
 
-        <li class="nav-item ">
-            <h6 style="text-align: center;color:antiquewhite">
-          <a class="nav-link active text-light" href="Rafsan_Home.php">
-            Home
-          </a>
-            </h6>
-        </li>
-        <li class="nav-item ">
-            <h6 style="text-align: center;color:antiquewhite">
-          <a class="nav-link active text-light" href="bookingpage.php">
-            Booking
-          </a>
-          </h6>
-        </li>
-      </ul>
-    </nav>
+                <li class="nav-item ">
+                    <h6 style="text-align: center;color:antiquewhite">
+                  <a class="nav-link active text-light" href="Rafsan_Home.php">
+                    Home
+                  </a>
+                    </h6>
+                </li>
+                <li class="nav-item ">
+                    <h6 style="text-align: center;color:antiquewhite">
+                  <a class="nav-link active text-light" href="bookingpage.php">
+                    Booking
+                  </a>
+                  </h6>
+                </li>
+              </ul>
+            </nav>
 
-        <div class = "container style="text-align: center; padding-top: 0.5cm;">
+                <div class = "container style="text-align: center; padding-top: 0.5cm;">
 
-            <div class="class text-center mb-5 text-primary">
-                <h6 style="text-align: center;color:antiquewhite">
-                   <p>
-                   Thank You Rafsan_1202202291 for Reserving
-                   <br>
-                   Please double check your reservation details
-                   </p> 
-                </h6>
+                    <div class="class text-center mb-5 text-primary">
+                        <h6 style="text-align: center;color:antiquewhite">
+                          <p>
+                          Thank You Rafsan_1202202291 for Reserving
+                          <br>
+                          Please double check your reservation details
+                          </p> 
+                        </h6>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">
-          <b>
-              Booking Number
-          </b>
-      </th>
-      <th scope="col">
-          <b>
-              Name
-          </b>
-      </th>
-      <th scope="col">
-          <b>
-              Check-in
-          </b>
-      </th>
-      <th scope="col">
-          <b>
-                Check-out
-          </b>
-      </th>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">
+                  <b>
+                      Booking Number
+                  </b>
+              </th>
+              <th scope="col">
+                  <b>
+                      Name
+                  </b>
+              </th>
+              <th scope="col">
+                  <b>
+                      Check-in
+                  </b>
+              </th>
+              <th scope="col">
+                  <b>
+                        Check-out
+                  </b>
+              </th>
 
-      <th scope="col">
-          <b>
-                Car Type
-          </b>
-      </th>
+              <th scope="col">
+                  <b>
+                        Car Type
+                  </b>
+              </th>
 
-      <th scope="col">
-          <b>
-                Phone Number
-          </b>
-      </th>
+              <th scope="col">
+                  <b>
+                        Phone Number
+                  </b>
+              </th>
 
-      <th scope="col">
-          <b>
-                Service(s)
-          </b>
-      </th>
+              <th scope="col">
+                  <b>
+                        Service(s)
+                  </b>
+              </th>
 
-      <th scope="col">
-          <b>
-                Total Price
-          </b>
-      </th>
+              <th scope="col">
+                  <b>
+                        Total Price
+                  </b>
+              </th>
 
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">
-          <?php 
-          echo rand();
-          ?></th>
-      <td>Muhammad Rizki Rafsyandjani</td>
-      <td>11-10-2022 10:00:00</td>
-      <td>12-10-2022 10:00:00</td>
-      <td>Alphard</td>
-      <td>081381796079</td>
-      <td>- Health protocol
-        <br>
-        - Fuel Filled
-      </td>
-      <td>Rp. 1.000.000</td>
-    </tr>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                  <?php 
+                  echo rand();
+                  ?></th>
+              <td>Muhammad Rizki Rafsyandjani</td>
+              <td>11-10-2022 10:00:00</td>
+              <td>12-10-2022 10:00:00</td>
+              <td>Alphard</td>
+              <td>081381796079</td>
+              <td>- Health protocol
+                <br>
+                - Fuel Filled
+              </td>
+              <td>Rp. 1.000.000</td>
+            </tr>
 
-  </tbody>
-</table>
-<footer style="text-align: center;color:black">
-    <b>
-        (Created by Rafsan_1202202291)
-    </b>
-</footer>
-</body>
-</html>
+          </tbody>
+        </table>
+        <footer style="text-align: center;color:black">
+            <b>
+                (Created by Rafsan_1202202291)
+            </b>
+        </footer>
+        </body>
+        </html>
